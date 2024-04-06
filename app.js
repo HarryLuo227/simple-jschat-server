7257092f943dfb45fd686a76a47eef7f2633ec59
HarryLuo227 <fanlos022753@gmail.com>
2024-04-06 23:08:53 +0800

    [chore]: Initialize a basic application and add .gitignore
    Initialize the project structure and add some features and files:
      - Add reading environment variable and logging features
      - Create app.js
      - Add .gitignore file

---
+++ app.js
@@ -0,0 +1,25 @@
+const config = require('./configs/config');
+const logger = require('./utils/logger');
+const morgan = require('morgan');
+const stream = {
+    write: (logMsg) => logger.http(logMsg)
+}
+const skip = () => {
+    return config.RunMode !== 'debug';
+}
+const express = require('express');
+const app = express();
+const path = require('path');
+const indexRouter = require('./routes/index');
+
+app.use(morgan('tiny', { stream, skip }));
+app.use('/', indexRouter);
+
+app.get('/', (req, res) => {
+    logger.debug('Get index page');
+    res.sendFile(path.join(__dirname, 'views/index.html'));
+});
+
+app.listen(config.ServerPort, () => {
+    logger.info(`Server is running on http://${config.ServerAddr}:${config.ServerPort} in ${config.RunMode} mode`);
+});
---
+++ configs/config.js
@@ -0,0 +1,7 @@
+const config = {
+    RunMode: process.env.RUN_MODE,
+    ServerAddr: process.env.SERVER_ADDRESS,
+    ServerPort: process.env.SERVER_PORT
+}
+
+module.exports = config;
