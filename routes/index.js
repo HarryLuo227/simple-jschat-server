f45e0c9b7520a91ebc3532639d7ea7c1a3b882f5
HarryLuo227 <fanlos022753@gmail.com>
2024-04-06 23:37:40 +0800

    [chore]: Add some routing rules of login and register feature
    Add some routing rules:
      - Add registry routing file
      - Add login routing file
      - Add users routing file

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const registryRouter = require('./registry');
+const loginRouter = require('./login');
+const apiRouter = require('./api/api');
+
+router.use('/registry', registryRouter);
+router.use('/login', loginRouter);
+router.use('/api', apiRouter);
 
 module.exports = router;
---
+++ routes/registry.js
@@ -0,0 +1,15 @@
+const logger = require('../utils/logger');
+const express = require('express');
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    try {
+        logger.debug('Get user registry page');
+        res.status(501).send('API is not implemented');
+    } catch (err) {
+        logger.error(`Unexpected error occurred: ${err}`);
+        res.status(404).send('Not found');
+    }
+});
+
+module.exports = router;
