c1e01129474895744b7aa31dcc32e92821e06320
HarryLuo227 <fanlos022753@gmail.com>
2024-04-09 14:33:10 +0800

    [feat]: Add users feature
    Add features about users:
      - Chore routes of users
      - Implement get all users info and get specific user info in users.js within services directory
      - Add route of entering user chatroom and views/chatroom.html

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -1,4 +1,74 @@
+const logger = require('../../../utils/logger');
 const express = require('express');
 const router = express.Router();
+const path = require('path');
+const userService = require('../../../services/users');
+
+router.get('/', async (req, res) => {
+    try {
+        logger.debug('List all users info');
+        const result = await userService.list(req, res);
+        switch(result.length) {
+            case 0:
+                const emptyResult = {};
+                res.status(200).json(emptyResult);
+                break;
+            case 1:
+                res.status(200).json(result[0]);
+                break;
+            default:
+                res.status(200).json(result);
+                break;
+        }
+    } catch (err) {
+        switch(err.message) {
+            case 'Db error':
+                logger.error(`Error caught in routes/users: ${err.message}`);
+                res.status(500).json({
+                    ErrorMsg: err.message
+                });
+                break;
+            default:
+                logger.error(`Error caught in routes/users: ${err}`);
+                res.status(500).json({
+                    ErrorMsg: err
+                });
+                break;
+        }
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        logger.debug('Get specific user info');
+        const result = await userService.get(req, res);
+        res.status(200).json(result);
+    } catch (err) {
+        switch(err.message) {
+            case 'Db error':
+                logger.error(`Error caught in routes/users: ${err.message}`);
+                res.status(404).json({
+                    ErrorMsg: err.message
+                });
+                break;
+            default:
+                logger.error(`Error caught in routes/users: ${err}`);
+                res.status(500).json({
+                    ErrorMsg: err
+                });
+                break;
+        }
+    }
+});
+
+router.get('/:id/chat', async (req, res) => {
+    try {
+        logger.debug('Enter chatroom');
+        res.status(200).sendFile(path.join(__dirname, '../../../views/chatroom.html'));
+    } catch (err) {
+        logger.error(`Error caught in routes/users: ${err}`);
+        res.status(404).end();
+    }
+});
 
 module.exports = router;
---
+++ services/users.js
@@ -0,0 +1,39 @@
+const logger = require('../utils/logger');
+const db = require('../db/index');
+
+async function list(req, res) {
+    try {
+        logger.debug('Get all users info');
+        const sql = 'SELECT * FROM users'
+        const result = await db.exec(sql);
+        return result.rows;
+    } catch (err) {
+        logger.error(`Error occurred in services/users: ${err}`);
+        throw err;
+    }
+}
+
+async function get(req, res) {
+    try {
+        logger.debug('Get user info');
+        const sql = 'SELECT * FROM users WHERE id = $1';
+        const values = [req.params.id];
+        const result = await db.exec(sql, values);
+        if(result) {
+            switch(result.rowCount) {
+                case 1:
+                    return result.rows[0];
+                default:
+                    throw new Error('Db error');
+            }
+        }
+    } catch (err) {
+        logger.error(`Error occurred in services/users: ${err}`);
+        throw err;
+    }
+}
+
+module.exports = {
+    list,
+    get
+}
